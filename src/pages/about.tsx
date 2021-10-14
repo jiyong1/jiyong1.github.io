89c5eeb6c43f64c2d8f8cfee9e16e8375862e196
jiyong1 <jyong9591@gmail.com>
2021-10-14 16:22:10 +0900

    about last section added

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import BaseLayout from '@layouts/base';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
 
+import BaseLayout from '@layouts/base';
 // about components
 import ScrollTop from '@components/about/ScrollTop';
 import Name from '@components/about/Name';
@@ -16,19 +18,59 @@ const About = (): JSX.Element => {
       <SectionFirst />
       <SectionSecond />
       <SectionThird />
-      <p
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          minHeight: '100vh',
-          fontSize: '2rem',
-        }}
-      >
-        감사합니다 😊
-      </p>
+      <AboutLastSection>
+        <p className="thank-text">감사합니다 😊</p>
+        <div className="button-container">
+          <Link className="bg-soft" to="/">
+            블로그 홈으로
+          </Link>
+          <Link className="bg-soft" to="/categories/">
+            게시물 보러가기
+          </Link>
+          <a className="bg-soft" href="https://github.com/jiyong1">
+            seventwo Github
+          </a>
+          <a className="bg-soft" href="https://mirror-lamb-c15.notion.site/c0736abeca144edab3de8b6d97ce6957">
+            seventwo Notion
+          </a>
+        </div>
+      </AboutLastSection>
     </BaseLayout>
   );
 };
+
+const AboutLastSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  .thank-text {
+    font-size: 2rem;
+    margin: 2em 0;
+  }
+  .button-container {
+    display: grid;
+    padding: 0 1rem;
+    grid-template-columns: 1fr 1fr;
+    width: 100%;
+    max-width: 600px;
+    grid-gap: 1.5rem;
+    justify-content: center;
+
+    > * {
+      font-size: 1.1rem;
+      height: 90px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 1rem;
+      &:hover {
+        color: inherit;
+        opacity: 0.7;
+      }
+    }
+  }
+`;
 
 export default About;
